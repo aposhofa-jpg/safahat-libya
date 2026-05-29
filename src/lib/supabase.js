be5cbdf517068ee0f15269ac8ac1bc7434e2527a
aposhofa-jpg <aposhofa@gmail.com>
2026-05-29 16:55:33 +0100

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,12 @@
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://uwnhtfkkpxtaopyfyweh.supabase.co";
+
+const supabaseKey =
+  "sb_publishable_rlnxq8zUpYNDzfi43pZ5Cw_RozRuyV6";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
